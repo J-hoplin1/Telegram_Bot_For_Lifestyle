a24858e85002c1bc5920914fe94a86aebc3287f6
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-21 14:42:03 +0900

    Update tutorial1.py

--- TelegramAPITutorial/tutorial1.py
+++ TelegramAPITutorial/tutorial1.py
@@ -7,7 +7,7 @@ def start(update,context):
 
 def startBot():
     #우선 처음에 Updater 객체를 생성해 주어야 한다. 이 객체에는 토큰이 위치해야한다
-    updater = Updater(token = '1687678296:AAEMuewFqJofLEu9YR8g6sVjAp0-rf9jWIM', use_context=True)
+    updater = Updater(token = '', use_context=True)
     dispatcher = updater.dispatcher
     #커맨드를 저장해보자. 목표는 start라는 함수를 /start command를 받을떄 마다 실행하는 것이다
     #이를 하기 위해서는 CommandHandler를 사용하여야 한다.
